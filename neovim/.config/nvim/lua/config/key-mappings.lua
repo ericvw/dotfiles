1cafabe5bcce606bc0766b21da2e21628741ae99
Eric N. Vander Weele <ericvw@gmail.com>
2021-09-23 11:05:33 -0400

    neovim: Remove <C-h/j/k/l> key mappings for window navigation
    While these key mappings are convenient for window navigation, I want to
    free them up for other usage (e.g., LSP key mappings).

--- neovim/.config/nvim/lua/config/key-mappings.lua
+++ neovim/.config/nvim/lua/config/key-mappings.lua
@@ -10,12 +10,6 @@ end
 map("n", "k", "gk")
 map("n", "j", "gj")
 
--- Easier window navigation/manipulation.
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-j>", "<C-w>j")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-l>", "<C-w>l")
-
 -- Disable highlight after search.
 map("", "<leader><space>", ":set hlsearch! <CR>")
 
