2a7342021f733cd5f48c66a052b17787d5576d50
Eric N. Vander Weele <ericvw@gmail.com>
2021-09-23 11:11:38 -0400

    neovim: Remove k/j -> g(k/j) key mappings for screen line navigation
    It is easy enough to hold `g` and press `k/j` to navigate by screen
    lines.

--- neovim/.config/nvim/lua/config/key-mappings.lua
+++ neovim/.config/nvim/lua/config/key-mappings.lua
@@ -6,10 +6,6 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
 end
 
--- Move up and down by screen lines.
-map("n", "k", "gk")
-map("n", "j", "gj")
-
 -- Disable highlight after search.
 map("", "<leader><space>", ":set hlsearch! <CR>")
 
