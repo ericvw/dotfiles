dc8cd90c19103d0557750a3e68872c2f58ec933c
Eric N. Vander Weele <ericvw@gmail.com>
2022-02-04 17:03:57 -0500

    neovim: Add packer to the runtime path for successful bootstrapping
    When running Neovim for the first time, plugins dynamically downloaded
    after startup aren't in the `runtimepath` because they didn't exist when
    `nvim` was invoked. To overcome this, manually adding the `packer`
    plugin to the runtime path when we realize we are bootstrapping the
    Neovim setup.

--- neovim/.config/nvim/lua/config/plugins.lua
+++ neovim/.config/nvim/lua/config/plugins.lua
@@ -1,10 +1,15 @@
 -- Plugins
 local fn = vim.fn
+local opt = vim.opt
 
 -- Bootstrap packer.
 local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+    -- XXX: Manually add to the runtime path when bootstraping because the
+    --      directory structure doesn't exist at Neovim startup to be
+    --      automatically added.
+    opt.runtimepath:prepend("/site/pack/*/start/*")
 end
 
 return require('packer').startup(function()
