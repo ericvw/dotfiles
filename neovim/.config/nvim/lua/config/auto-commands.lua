1354f078919b9ee4ea7cd06371133d76c072dd90
Eric N. Vander Weele <ericvw@gmail.com>
2022-08-10 17:19:04 -0400

    neovim: Use one shot BufRead workaround for restoring the cursor
    When initially porting the Vimscript auto commands to Lua, I introduced
    a bug that would move the cursor to the last opened position when
    switching between windows. The root cause was using the `BufEnter` event
    that was firing every time. I cheekily used `BufEnter` because the
    buffer `filetype` was set when fired, whereas it is not set when
    `BufRead` is called. In short, this is happening because Neovim
    `:filetype plugin indent on` is being executed by default *after* the
    user startup scripts.
    
    To workaround this and desiring to keep everything in Lua, I ported a
    Vimscript workaround provided upstream to Lua. It's definitely more
    complicated and verbose than the Vimscript equivalent. However, it works
    like a charm :).

--- neovim/.config/nvim/lua/config/auto-commands.lua
+++ neovim/.config/nvim/lua/config/auto-commands.lua
@@ -1,20 +1,40 @@
 -- Automatic commands
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 local fn = vim.fn
 local opt = vim.opt
 
 -- Jump to the last cursor position in the file if possible.
 -- TODO: :help restore-cursor uses BufRead.
---       However, &ft is not set when BufRead is triggered.
-autocmd("BufEnter", {
+--       However, &ft is not set when BufRead is triggered, and we need to
+--       workaround this unto Neovim addresses this upstream.
+--       See: https://github.com/neovim/neovim/issues/15536.
+local function is_within_document(line)
+    return line >= 1 and line <= fn.line("$")
+end
+
+local function is_git_filetype(ft)
+    return string.find(ft, "commit") or string.find(ft, "rebase")
+end
+
+local restore_cursor = augroup("RestoreCursor", {})
+autocmd("BufRead", {
+    group = restore_cursor,
     pattern = "*",
     callback = function(args)
-        local ft = opt.filetype:get()
+        autocmd("FileType", {
+            buffer = args.buffer,
+            once = true,
+            callback = function(args)
+                local ft = opt.filetype:get()
+                local line = fn.line("'\"")
 
-        local line = fn.line("'\"")
-        if line >= 1 and line <= fn.line("$") and not string.find(ft, "commit") then
-            fn.setpos(".", fn.getpos("'\""))
-        end
+                if is_within_document(line) and not is_git_filetype(ft) then
+                    fn.setpos(".", fn.getpos("'\""))
+                end
+            end,
+            desc = "Jump to last cursor position in the file if possible",
+        })
     end,
-    desc = "Jump to last cursor position in the file if possible",
+    desc = "Create one shot buffer-local autocommand to restore cursor",
 })
