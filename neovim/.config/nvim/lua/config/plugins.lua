7986e4540f9e3a71468402406fd8b4b56a65a2ee
Eric N. Vander Weele <ericvw@gmail.com>
2023-03-29 12:31:36 -0400

    neovim: Track the 0.1.x release branch for the Telescope plugin
    Instead of being pinned to a particular version, ensure that updates can
    be absorbed.

--- neovim/.config/nvim/lua/config/plugins.lua
+++ neovim/.config/nvim/lua/config/plugins.lua
@@ -39,7 +39,7 @@ require('packer').startup(function()
     }
     use {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.0",
+        branch = "0.1.x",
         requires = {
             "nvim-lua/plenary.nvim",
         },
