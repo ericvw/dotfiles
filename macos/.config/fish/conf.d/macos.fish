d9c4e4c8aab399e65e560433d7df5413795d7669
Eric N. Vander Weele <ericvw@gmail.com>
2024-10-04 12:07:08 -0400

    macos: Add macos
    The macOS environment requires additional configuration to layer on top
    of the application-specific dotfile stow packages. Specifically…
    
    * fish
      * Configure Homebrew and paths to Homebrew-installed packages.
      * Set alternative path to find tmux-256color provided by my dotfiles
        for the tmux stow package.
    * git
      * Use Git Credential Manager installed for macOS.

---
+++ macos/.config/fish/conf.d/macos.fish
@@ -0,0 +1,21 @@
+if not status is-interactive
+    exit
+end
+
+# Don't automatically run `brew update` before running other commands.
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
+
+# Set environment variables for Homebrew.
+/opt/homebrew/bin/brew shellenv | source
+
+## XXX: Ensure -p|--path is used to ensure `pyenv` paths are prepended to final PATH.
+set -l brew_prefix (brew --prefix)
+fish_add_path -P -m $brew_prefix/opt/make/libexec/gnubin
+fish_add_path -P -m $brew_prefix/opt/python/libexec/bin
+fish_add_path -P -m $brew_prefix/opt/ccache/libexec
+fish_add_path -P -m $brew_prefix/opt/curl/bin
+fish_add_path -P -m $brew_prefix/opt/findutils/libexec/gnubin
+fish_add_path -P -m $brew_prefix/opt/coreutils/libexec/gnubin
+
+# Alternative search path for dotfiles tmux-generated tmux-256color terminfo.
+set -gx TERMINFO_DIRS $HOME/.local/share/terminfo
