a6d408e97c61aa36c8bd84e14b5e109627ecc1c6
Eric N. Vander Weele <ericvw@gmail.com>
2024-11-12 11:58:57 -0500

    neovim: Split out lsp-related lazy.nvim plugin specifications
    Consolidate the specification and configuration for lspconfig and
    lsp_signature into one location.

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,10 +1,6 @@
 -- lazy.nvim plugin specification
 
 return {
-    -- Completion/LSP plugins.
-    "neovim/nvim-lspconfig",
-    "ray-x/lsp_signature.nvim",
-
     -- IDE/UI plugins.
     "dense-analysis/ale",
 }
