0cd53ae3204e285ab7c6936ca86fdc64173e55ab
Eric N. Vander Weele <ericvw@gmail.com>
2024-11-12 13:01:47 -0500

    neovim: Update LSP configuration to latest `:h lsp-config standards`
    Match configuration according to `:h lsp-config. The significant changes
    are:
    
    * Rename autocommand argument from 'ev' to 'args'.
    * Reformat indentation from 2 to 4 spaces.

--- neovim/.config/nvim/lua/config/lsp.lua
+++ neovim/.config/nvim/lua/config/lsp.lua
@@ -3,21 +3,20 @@
 -- Use LspAttach autocommand to only map the following keys after the language
 -- server attaches to the current buffer.
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    -- Buffer local mappings
-
-    -- See `:help vim.lsp.*` for documentation on any of the below functions.
-    local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<space>f", function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
+    callback = function(args)
+        -- Buffer local mappings
+        -- See `:help vim.lsp.*` for documentation on any of the below functions.
+        local opts = { buffer = args.buf }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<space>f", function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
 })
