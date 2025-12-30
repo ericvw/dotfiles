7be465ac2e3aed8894508f727cecdae3ae0c4def
Eric N. Vander Weele <ericvw@gmail.com>
2025-12-30 13:22:18 -0500

    neovim: Update nvim-treesitter to 'main' branch
    The 'main' branch of the nvim-treesitter plugin is a complete rewrite
    and focuses purely on parser management. With the rewrite removing the
    convenience of enabling the treesitter syntax highlighting, I am
    removing the pre-installed parsers, as I need to revisit my overall
    configuration Neovim configuration to ensure I am using treesitter
    syntax highlighting for the languages  I care about.

--- neovim/.config/nvim/lua/plugins/tree-sitter.lua
+++ neovim/.config/nvim/lua/plugins/tree-sitter.lua
@@ -3,31 +3,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        lazy = false,
         build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.install").prefer_git = true
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    "cpp",
-                    "fish",
-                    "go",
-                    "json",
-                    "lua",
-                    "make",
-                    "nim",
-                    "python",
-                    "rust",
-                    "toml",
-                    "vim",
-                    "vimdoc",
-                },
-                sync_install = #vim.api.nvim_list_uis() == 0,
-                highlight = {
-                    enable = true,
-                },
-            })
-        end,
     },
 }
